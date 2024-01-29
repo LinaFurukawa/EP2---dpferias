d79d1eecbff33e52e4c2f33867e3344d0c89b1f3
LinaFurukawa <linafq@al.Insper.edu.br>
2024-01-29 00:40:20 -0300

    Refinamento da lógica do loop do jogo, ajuste da validacao do ataque_linha e ataque_coluna e adição da jogada do oponente no loop principal.

--- Jogo_Batalha_Naval.py
+++ Jogo_Batalha_Naval.py
@@ -14,45 +14,49 @@ while jogando:
     # Imprimindo tabuleiro
     print(monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente))
 
-    coordenada_repetida = True
-    while coordenada_repetida:
-
-        # TODO: Implemente aqui a lógica para perguntar a linha que o jogador deseja atirar
-        linha_invalida = True
-        while linha_invalida:
-            ataque_linha = input("Insira a linha que deseja atirar: ")
-            if ataque_linha.isdigit(): 
-                ataque_linha = int(ataque_linha)
-                if ataque_linha < 0 or ataque_linha > 9:
-                    print('Linha inválida!')
-                else:
-                    linha_invalida = False
-            else:
-                print('Linha inválida!')
+    # TODO: Implemente aqui a lógica para perguntar a linha que o jogador deseja atirar
+    ataque_linha = input("Insira a linha que deseja atirar: ")
+    if ataque_linha.isdigit(): 
+        ataque_linha = int(ataque_linha)
+        if ataque_linha < 0 or ataque_linha > 9:
+            print('Linha inválida!')
 
         # TODO: Implemente aqui a lógica para perguntar a coluna que o jogador deseja atirar
-        coluna_invalida = True
-        while coluna_invalida:
+        else:
             ataque_coluna = input("Insira a coluna que deseja atirar: ")
             if ataque_coluna.isdigit(): 
                 ataque_coluna = int(ataque_coluna)
                 if ataque_coluna < 0 or ataque_coluna > 9:
                     print('Coluna inválida!')
+
+                # TODO: Implemente aqui a lógica para verificar se a linha e coluna não foram escolhidas anteriormente
                 else:
-                    coluna_invalida = False
+                    if tabuleiro_oponente[ataque_linha][ataque_coluna] != 0 and tabuleiro_oponente[ataque_linha][ataque_coluna] != 1:
+                        print(f'A posição linha {ataque_linha} e coluna {ataque_coluna} já foi informada anteriormente!')
+
+                    else:
+                        tabuleiro_oponente = faz_jogada(tabuleiro_oponente,ataque_linha,ataque_coluna)
+
+                        # TODO: Implemente aqui a lógica para verificar se o jogador derrubou todos os navios do oponente
+                        navios_afundados = afundados(frota_oponente,tabuleiro_oponente)
+                        if navios_afundados == len(frota_oponente):
+                            print('Parabéns! Você derrubou todos os navios do seu oponente!')
+                            jogando = False
+
+                        # TODO: Implemente aqui a lógica para que o oponente jogue caso ele ainda tenha navios                 
+                        else:
+                            while True:
+                                ataque_linha_oponente = random.randint(0,9)
+                                ataque_coluna_oponente = random.randint(0,9)
+                                if tabuleiro_jogador[ataque_linha_oponente][ataque_coluna_oponente] == 0 or tabuleiro_jogador[ataque_linha_oponente][ataque_coluna_oponente] == 1:
+                                    tabuleiro_jogador = faz_jogada(tabuleiro_jogador,ataque_linha_oponente,ataque_coluna_oponente)
+                                    navios_afundados_oponente = afundados(frota_jogador,tabuleiro_jogador)
+                                    print(f'Seu oponente está atacando na linha {ataque_linha_oponente} e coluna {ataque_coluna_oponente}')
+                                    if navios_afundados_oponente == len(frota_jogador):
+                                        print('Xi! O oponente derrubou toda a sua frota =(')
+                                        jogando = False
+                                    break
             else:
-                    print('Coluna inválida!')
-
-        # TODO: Implemente aqui a lógica para verificar se a linha e coluna não foram escolhidas anteriormente
-        if tabuleiro_oponente[ataque_linha][ataque_coluna] != 0 and tabuleiro_oponente[ataque_linha][ataque_coluna] != 1:
-            print(f'A posição linha {ataque_linha} e coluna {ataque_coluna} já foi informada anteriormente!')
-        else:
-            coordenada_repetida = False
-
-    tabuleiro_oponente = faz_jogada(tabuleiro_oponente,ataque_linha,ataque_coluna)
-
-    # TODO: Implemente aqui a lógica para verificar se o jogador derrubou todos os navios do oponente
-    navios_afundados = afundados(frota_oponente,tabuleiro_oponente)
-    if navios_afundados == len(frota_oponente):
-        print('Parabéns! Você derrubou todos os navios do seu oponente!')
-        jogando = False
+                print('Coluna inválida!')
+    else:
+        print('Linha inválida!')
