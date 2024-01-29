1934410cd2bf44249f36305acac62098a7cb0a86
LinaFurukawa <linafq@al.Insper.edu.br>
2024-01-29 00:35:30 -0300

    Adição da função: Posicao Valida

--- Funcoes.py
+++ Funcoes.py
@@ -94,6 +94,24 @@ def preenche_frota(dados_posicion,tipo_navio,frota):
 
     return frota
 
+def posiciona_frota(frota):
+
+    tabuleiro = []
+
+    for linha in range(10):
+        tabuleiro.append([])
+        for coluna in range(10):
+            tabuleiro[linha].append(0)
+
+    for navio in frota:
+        for posicoes in navio['posicoes']:
+            linha_navio = posicoes[0]
+            coluna_navio = posicoes[1]
+
+            tabuleiro[linha_navio][coluna_navio] = 1
+
+    return tabuleiro
+
 def define_posicoes(dados_posicao):
 
     linha = dados_posicao["linha"]
@@ -112,29 +130,23 @@ def define_posicoes(dados_posicao):
 
     return posicao
 
-def preenche_frota(dados_posicion,tipo_navio,frota):
+def posicao_valida(dados_posicao,frota):
 
-    linha = dados_posicion['linha']
-    coluna = dados_posicion['coluna']
-    orientacao = dados_posicion['orientacao']
-    tamanho = dados_posicion['tamanho']
+    novo_navio = define_posicoes(dados_posicao)
 
-    novo_barco = {}
-    novo_barco['tipo'] = tipo_navio
-    novo_barco['posicoes'] = [[linha,coluna]]
+    for posicao_novo_navio in novo_navio:
 
-    for i in range(tamanho-1):
+        linha_novo_navio = posicao_novo_navio[0]
+        coluna_novo_navio = posicao_novo_navio[1]
+        if linha_novo_navio > 9 or coluna_novo_navio > 9:
+            return False
 
-        if orientacao == "horizontal":
-            coluna += 1
-        if orientacao == "vertical":
-            linha += 1
-        
-        novo_barco['posicoes'].append([linha,coluna])
-
-    frota.append(novo_barco)
-
-    return frota
+        for navio in frota:
+            for posicao in navio['posicoes']:
+                if posicao == posicao_novo_navio:
+                    return False
+    
+    return True
 
 def faz_jogada(tabuleiro,linha,coluna):
     
@@ -145,23 +157,6 @@ def faz_jogada(tabuleiro,linha,coluna):
     
     return tabuleiro
 
-def posiciona_frota(frota):
-
-    tabuleiro = []
-
-    for linha in range(10):
-        tabuleiro.append([])
-        for coluna in range(10):
-            tabuleiro[linha].append(0)
-
-    for navio in frota:
-        for posicoes in navio['posicoes']:
-            linha_navio = posicoes[0]
-            coluna_navio = posicoes[1]
-
-            tabuleiro[linha_navio][coluna_navio] = 1
-
-    return tabuleiro
 
 def afundados(frota,tabuleiro):
 
